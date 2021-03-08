f192d633836ba43e34e430a1d6d80792417930e4
Anita9771 <ieghonghon@gmail.com>
2021-03-08 17:29:42 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/screen_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.screen_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+  void main() => runApp(MaterialApp(
+    home: home{},
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Annie'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Icon(
+          Icons.
+        )
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+}
